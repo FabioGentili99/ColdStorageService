adbfb80672ea10400a8fb68a92d7ee5efe10b0e8
FabioGentili99 <fabiogentili20@gmail.com>
2023-10-25 17:54:47 +0200

    qak completo sprint 1

--- sprint1/sprint1_analisiproblema/Arch_coldstorageservice.py
+++ sprint1/sprint1_analisiproblema/Arch_coldstorageservice.py
@@ -1,3 +1,4 @@
+### conda install diagrams
 from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 import os
@@ -19,6 +20,19 @@ eventedgeattr = {
 with Diagram('coldstorageserviceArch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctxcoldstorageservice', graph_attr=nodeattr):
+          coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
+          coldroommanager=Custom('coldroommanager','./qakicons/symActorSmall.png')
+          ticketmanager=Custom('ticketmanager','./qakicons/symActorSmall.png')
+          transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
      with Cluster('ctxbasicrobot', graph_attr=nodeattr):
           basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxaccessgui', graph_attr=nodeattr):
+          serviceaccessgui_simulator=Custom('serviceaccessgui_simulator','./qakicons/symActorSmall.png')
+     transporttrolley >> Edge(color='magenta', style='solid', decorate='true', label='<engage &nbsp; moverobot &nbsp; >',  fontcolor='magenta') >> basicrobot
+     serviceaccessgui_simulator >> Edge(color='magenta', style='solid', decorate='true', label='<storerequest &nbsp; verifyticket &nbsp; loaddone &nbsp; >',  fontcolor='magenta') >> coldstorageservice
+     coldroommanager >> Edge(color='magenta', style='solid', decorate='true', label='<generateticket &nbsp; >',  fontcolor='magenta') >> ticketmanager
+     transporttrolley >> Edge(color='blue', style='solid',  label='<depositfailed &nbsp; depositdone &nbsp; >',  fontcolor='blue') >> coldroommanager
+     ticketmanager >> Edge(color='blue', style='solid',  label='<expiredticket &nbsp; >',  fontcolor='blue') >> coldroommanager
 diag
