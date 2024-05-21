a8dc865c1bdfddce53566f1ca93e8914f3c7ee28
FabioGentili99 <fabiogentili20@gmail.com>
2024-05-21 20:01:38 +0200

    sprint 3 progetto cold storage service

---
+++ sprint3/sprint3_progetto/Arch_coldstorageservice.py
@@ -0,0 +1,48 @@
+### conda install diagrams
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+with Diagram('coldstorageserviceArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctxcoldstorageservice', graph_attr=nodeattr):
+          coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
+          coldroommanager=Custom('coldroommanager','./qakicons/symActorSmall.png')
+          ticketmanager=Custom('ticketmanager','./qakicons/symActorSmall.png')
+          transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
+          planexec=Custom('planexec','./qakicons/symActorSmall.png')
+          robotpos=Custom('robotpos','./qakicons/symActorSmall.png')
+          basicrobot=Custom('basicrobot','./qakicons/symActorSmall.png')
+     with Cluster('ctxraspberrypi', graph_attr=nodeattr):
+          warningdevice=Custom('warningdevice','./qakicons/symActorSmall.png')
+          alarmdevice=Custom('alarmdevice','./qakicons/symActorSmall.png')
+          warningdevice=Custom('warningdevice(ext)','./qakicons/externalQActor.png')
+          alarmdevice=Custom('alarmdevice(ext)','./qakicons/externalQActor.png')
+     transporttrolley >> Edge(color='magenta', style='solid', decorate='true', label='<moverobot<font color="darkgreen"> moverobotdone moverobotfailed</font> &nbsp; >',  fontcolor='magenta') >> robotpos
+     robotpos >> Edge(color='magenta', style='solid', decorate='true', label='<doplan<font color="darkgreen"> doplandone doplanfailed</font> &nbsp; >',  fontcolor='magenta') >> planexec
+     planexec >> Edge(color='magenta', style='solid', decorate='true', label='<step<font color="darkgreen"> stepdone stepfailed</font> &nbsp; >',  fontcolor='magenta') >> basicrobot
+     coldroommanager >> Edge(color='magenta', style='solid', decorate='true', label='<generateticket &nbsp; >',  fontcolor='magenta') >> ticketmanager
+     basicrobot >> Edge(color='blue', style='solid',  label='<robotready &nbsp; >',  fontcolor='blue') >> transporttrolley
+     transporttrolley >> Edge(color='blue', style='solid',  label='<updateled &nbsp; >',  fontcolor='blue') >> warningdevice
+     planexec >> Edge(color='blue', style='solid',  label='<nextmove &nbsp; nomoremove &nbsp; >',  fontcolor='blue') >> planexec
+     transporttrolley >> Edge(color='blue', style='solid',  label='<depositdone &nbsp; >',  fontcolor='blue') >> coldroommanager
+     transporttrolley >> Edge(color='blue', style='solid',  label='<stopplan &nbsp; continueplan &nbsp; >',  fontcolor='blue') >> planexec
+     transporttrolley >> Edge(color='blue', style='solid',  label='<setrobotstate &nbsp; setdirection &nbsp; >',  fontcolor='blue') >> robotpos
+     ticketmanager >> Edge(color='blue', style='solid',  label='<expiredticket &nbsp; >',  fontcolor='blue') >> coldroommanager
+diag
---
+++ sprint3/sprint3_progetto/coldstorageservice.pl
@@ -0,0 +1,55 @@
+%====================================================================================
+% coldstorageservice description   
+%====================================================================================
+request( getweight, getweight(NO_PARAM) ).
+reply( currentweight, currentweight(CurrentWeight,FreeSpace) ).  %%for getweight
+request( storerequest, storerequest(TruckLoad) ).
+reply( loadaccepted, loadaccepted(Ticket,FreeSpace) ).  %%for storerequest
+reply( loadrejected, loadrejected(_) ).  %%for storerequest
+request( verifyticket, verifyticket(TruckTicket) ).
+reply( chargetaken, chargetaken(_) ).  %%for verifyticket
+reply( invalidticket, invalidticket(REASON) ).  %%for verifyticket
+request( loaddone, loaddone(TruckTicket) ).
+reply( goaway, goaway(_) ).  %%for loaddone
+reply( depositfailed, depositfailed(_) ).  %%for loaddone
+request( generateticket, generateticket(TruckLoad) ).
+dispatch( depositdone, depositdone(Ticket) ).
+dispatch( depositfailed, depositfailed(Ticket) ).
+dispatch( expiredticket, expiredticket(Ticket) ).
+dispatch( cmd, cmd(MOVE) ).
+dispatch( end, end(ARG) ).
+request( step, step(TIME) ).
+reply( stepdone, stepdone(V) ).  %%for step
+reply( stepfailed, stepfailed(DURATION,CAUSE) ).  %%for step
+request( doplan, doplan(PATH,STEPTIME) ).
+reply( doplandone, doplandone(ARG) ).  %%for doplan
+reply( doplanfailed, doplanfailed(ARG) ).  %%for doplan
+dispatch( nextmove, nextmove(M) ).
+dispatch( nomoremove, nomoremove(M) ).
+dispatch( robotready, robotready(BOOL) ).
+dispatch( setdirection, dir(D) ).
+request( moverobot, moverobot(TARGETX,TARGETY) ).
+reply( moverobotdone, moverobotok(ARG) ).  %%for moverobot
+reply( moverobotfailed, moverobotfailed(PLANDONE,PLANTODO) ).  %%for moverobot
+request( getrobotstate, getrobotstate(ARG) ).
+reply( robotstate, robotstate(POS,DIR) ).  %%for getrobotstate
+dispatch( stopplan, stopplan(NO_PARAM) ).
+dispatch( continueplan, continueplan(NO_PARAM) ).
+dispatch( setrobotstate, setpos(X,Y,D) ).
+dispatch( stop, stop(_) ).
+dispatch( resume, resume(_) ).
+dispatch( updateled, updateled(RobotState) ).
+%====================================================================================
+context(ctxcoldstorageservice, "localhost",  "TCP", "9990").
+context(ctxraspberrypi, "localhost",  "TCP", "9999").
+ qactor( warningdevice, ctxraspberrypi, "external").
+  qactor( alarmdevice, ctxraspberrypi, "external").
+  qactor( coldstorageservice, ctxcoldstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
+  qactor( coldroommanager, ctxcoldstorageservice, "it.unibo.coldroommanager.Coldroommanager").
+  qactor( ticketmanager, ctxcoldstorageservice, "it.unibo.ticketmanager.Ticketmanager").
+  qactor( transporttrolley, ctxcoldstorageservice, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( planexec, ctxcoldstorageservice, "it.unibo.planexec.Planexec").
+  qactor( robotpos, ctxcoldstorageservice, "it.unibo.robotpos.Robotpos").
+  qactor( basicrobot, ctxcoldstorageservice, "it.unibo.basicrobot.Basicrobot").
+  qactor( warningdevice, ctxraspberrypi, "it.unibo.warningdevice.Warningdevice").
+  qactor( alarmdevice, ctxraspberrypi, "it.unibo.alarmdevice.Alarmdevice").
